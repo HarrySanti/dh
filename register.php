863ba8b5089598ea404a07967e306b0e40cefe9e
Santi <mellosantiago@gmail.com>
2018-05-22 09:34:20 -0300

    Update register.php

--- register.php
+++ register.php
@@ -262,7 +262,11 @@
 								<option value="1997">1997</option>
 								<option value="1996">1996</option>
 								<option value="1995">1995</option>
-								<option value="1995">1994</option>
+								<option value="1994">1994</option>
+								<option value="1993">1993</option>
+								<option value="1992">1992</option>
+								<option value="1991">1991</option>
+								<option value="1990">1990</option>
 							</select>
 						</div>
 					</div>
